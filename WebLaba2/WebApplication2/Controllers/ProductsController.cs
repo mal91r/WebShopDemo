d3ece1d31a2c2447e6b4103325e28909a026fcfd
Artem Malanin <malanin.artem@mail.ru>
2022-03-11 23:48:52 +0300

    downloaded

---
+++ WebLaba2/WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(ProductService.GetProducts());
+        }
+
+        [Route("product/{id}")]
+        public IActionResult Product(string id) =>
+            View("ViewItem", ProductService.GetProducts().First(x => x.Id == id));
+    }
+}
---
+++ WebLaba2/WebApplication2/Services/JsonFileProductService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class JsonFileProductService
+{
+    public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+    {
+        WebHostEnvironment = webHostEnvironment;
+    }
+
+    public IWebHostEnvironment WebHostEnvironment { get; }
+
+    private string JsonFileName =>
+        Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+
+    public IEnumerable<Product> GetProducts()
+    {
+        using (var jsonFileReader = File.OpenText(JsonFileName))
+        {
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+    }
+}
